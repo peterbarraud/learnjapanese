3ee4e947f2f4bec8331d40e3dedc018c47f4ea71
Peter Barraud <barraud@adobe.com>
2020-07-12 19:17:26 +0530

    katakana support

--- index.js
+++ index.js
@@ -1,51 +1,77 @@
 $( document ).ready(function() {
 
     var characterSet = null;
-    
-    $( '#learn_hiragana' ).prop('disabled', false);
-    $( '#learn_kanji' ).prop('disabled', false);
+    $("[name='learn']").prop('disabled', false);
     $(".form-signin").submit(function(e){
         e.preventDefault();
     });
 
+    $( '#home' ).click(function(){
+        $( '#home' ).attr('height',"300px");
+        $( "[name='learn']" ).show();
+        $( '#movenext, #moveprevious, #movefirst, #movelast, #char_pic, .alert'  ).hide();
+    })
 
-    $( '#learn_hiragana' ).click(function(event){
-        characterSet = new Hiragana();
-        $( this ).hide();
-        $( '#movenext' ).show();
-        $( '#moveprevious' ).show();
-        $( '#char_pic' ).show();
-        $( '.alert' ).show();
+    $( "[name='learn']" ).click(function(event){
+        if ($( this ).attr('characterClass') === 'Hiragana'){
+            characterSet = new Hiragana();
+        } else if ($( this ).attr('characterClass') === 'Katakana'){
+            characterSet = new Katakana();
+        } else if ($( this ).attr('characterClass') === 'Kanji'){
+            characterSet = new Kanji();
+        }
+        $( '#home' ).attr('height',"50px");
+        $( "[name='learn']" ).hide();
+
+        $( '#movenext, #moveprevious, #movefirst, #movelast, #char_pic, .alert'  ).show();
         characterSet.first();
         $( '#char_pic' ).prop('src',characterSet.char);
         $( '#syllable' ).text(characterSet.syllable);
+        $( '#moveprevious' ).prop('disabled', characterSet.isfirstCharacter);
+    });
+    // F-N-L-P
+    $( '#movefirst').click(function(event){
+        characterSet.first();
+        $( '#char_pic' ).prop('src',characterSet.char);
+        $( '#syllable' ).text(characterSet.syllable);
+        $( '#moveprevious' ).prop('disabled', characterSet.isfirstCharacter);
+        
     });
 
     $( '#movenext').click(function(event){
         characterSet.next();
         $( '#char_pic' ).prop('src',characterSet.char);
         $( '#syllable' ).text(characterSet.syllable);
-        
+        $( '#moveprevious' ).prop('disabled', characterSet.isfirstCharacter);
     })
 
-    $( '#moveprevious').click(function(event){
-        characterSet.previous();
+    $( '#movelast').click(function(event){
+        characterSet.last();
         $( '#char_pic' ).prop('src',characterSet.char);
         $( '#syllable' ).text(characterSet.syllable);
-        
+        $( '#moveprevious' ).prop('disabled', characterSet.isfirstCharacter);
+    })
+
+    $( '#moveprevious' ).click(function(event){
+        var pos = characterSet.previous();
+        $( '#char_pic' ).prop('src',characterSet.char);
+        $( '#syllable' ).text(characterSet.syllable);
+        $( '#moveprevious' ).prop('disabled', characterSet.isfirstCharacter);   
     })
 
     $(document).keypress(function(event) {
         if ( event.which == 110 || event.which == 78 ) {
             characterSet.next();
+            $( '#moveprevious' ).prop('disabled', characterSet.isfirstCharacter);
         } else if ( event.which === 112 || event.which === 78 ) { // p for prev
             characterSet.previous();
-    
+            $( '#moveprevious' ).prop('disabled', characterSet.isfirstCharacter);
         } else if ( event.which === 102 || event.which === 70 ) { // f for first
             characterSet.first();
-    
+            $( '#moveprevious' ).prop('disabled', characterSet.isfirstCharacter);
         } else if ( event.which === 108 || event.which === 76 ) { // l for last
             characterSet.last();
+            $( '#moveprevious' ).prop('disabled', characterSet.isfirstCharacter);
         }
         $( '#char_pic' ).prop('src',characterSet.char);
         $( '#syllable' ).text(characterSet.syllable);
--- index.lib.js
+++ index.lib.js
@@ -1,40 +1,70 @@
-class Hiragana {
-    constructor() {
-        this.character_set = getHiraganaJson();
-        this.cursor_pos = 0;
-        this.cursor = [];
+class Japanese {
+    #character_set = null;
+    #cursor_pos = 0;
+    #cursor = [];
+    constructor(character_set) {
+        this.#character_set = character_set;
         for (var i=0;i<48;i++){
-            this.cursor.push(i);
+            this.#cursor.push(i);
         }
-        this.char = this.character_set[this.cursor[this.cursor_pos]].char;
-        this.syllable = this.character_set[this.cursor[this.cursor_pos]].syllable;
+        this.char = this.#character_set[this.#cursor[this.#cursor_pos]].char;
+        this.syllable = this.#character_set[this.#cursor[this.#cursor_pos]].syllable;
+        this.isfirstCharacter = this.#cursor_pos === 0;
+        this.islastCharacter = this.#character_set.length-1;  
     }
+
     next(){
-        this.cursor_pos +=1;
-        if (this.cursor_pos === this.character_set.length){
-            this.cursor_pos = 0;
+        this.#cursor_pos +=1;
+        if (this.#cursor_pos === this.#character_set.length){
+            this.#cursor_pos = 0;
         }
-        this.char = this.character_set[this.cursor[this.cursor_pos]].char;
-        this.syllable = this.character_set[this.cursor[this.cursor_pos]].syllable;        
-        
+        this.char = this.#character_set[this.#cursor[this.#cursor_pos]].char;
+        this.syllable = this.#character_set[this.#cursor[this.#cursor_pos]].syllable;     
+        this.isfirstCharacter = this.#cursor_pos === 0;
+        this.islastCharacter = this.#character_set.length-1;  
     }
     previous(){
-        this.cursor_pos -= 1;
-        if (this.cursor_pos === -1){
-            this.cursor_pos = this.character_set.length-1;
+        this.#cursor_pos -= 1;
+        if (this.#cursor_pos === -1){
+            // don't move to end
+            this.#cursor_pos = 0;
         }
-        this.char = this.character_set[this.cursor[this.cursor_pos]].char;
-        this.syllable = this.character_set[this.cursor[this.cursor_pos]].syllable;        
+        this.char = this.#character_set[this.#cursor[this.#cursor_pos]].char;
+        this.syllable = this.#character_set[this.#cursor[this.#cursor_pos]].syllable;
+        this.isfirstCharacter = this.#cursor_pos === 0;
+        this.islastCharacter = this.#character_set.length-1;  
     }
     first(){
-        this.cursor_pos = 0;
-        this.char = this.character_set[this.cursor[this.cursor_pos]].char;
-        this.syllable = this.character_set[this.cursor[this.cursor_pos]].syllable;        
+        this.#cursor_pos = 0;
+        this.char = this.#character_set[this.#cursor[this.#cursor_pos]].char;
+        this.syllable = this.#character_set[this.#cursor[this.#cursor_pos]].syllable;   
+        this.isfirstCharacter = this.#cursor_pos === 0;
+        this.islastCharacter = this.#character_set.length-1;  
     }
     last(){
-        this.cursor_pos = this.character_set.length-1;
-        this.char = this.character_set[this.cursor[this.cursor_pos]].char;
-        this.syllable = this.character_set[this.cursor[this.cursor_pos]].syllable;        
+        this.#cursor_pos = this.#character_set.length-1;
+        this.char = this.#character_set[this.#cursor[this.#cursor_pos]].char;
+        this.syllable = this.#character_set[this.#cursor[this.#cursor_pos]].syllable;
+        this.isfirstCharacter = this.#cursor_pos === 0;
+        this.islastCharacter = this.#character_set.length-1;  
+    }
+}
+
+class Hiragana extends Japanese {
+    constructor() {
+        super(getHiraganaJson());
     }
 
   }
+class Katakana extends Japanese {
+    constructor() {
+        super(getKatakanaJson());
+    }
+
+  }  
+  class Kanji extends Japanese {
+    constructor() {
+        super(getKanjiJson());
+    }
+
+  } 
