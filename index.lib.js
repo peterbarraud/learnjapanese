2e00e9f0697d6012b544b1088dfb4c33ba892015
Peter Barraud <barraud@adobe.com>
2020-07-12 19:31:38 +0530

    reverting because extends syntax may not work on mobile browsers

--- index.lib.js
+++ index.lib.js
@@ -1,6 +1,7 @@
-class Japanese {
-    constructor(character_set) {
-        this.character_set = character_set;
+
+class Hiragana {
+    constructor() {
+        this.character_set = getHiraganaJson();
         this.cursor_pos = 0;
         this.cursor = [];
         for (var i=0;i<48;i++){
@@ -8,19 +9,15 @@ class Japanese {
         }
         this.char = this.character_set[this.cursor[this.cursor_pos]].char;
         this.syllable = this.character_set[this.cursor[this.cursor_pos]].syllable;
-        this.isfirstCharacter = this.cursor_pos === 0;
-        this.islastCharacter = this.character_set.length-1;  
     }
-
     next(){
         this.cursor_pos +=1;
         if (this.cursor_pos === this.character_set.length){
             this.cursor_pos = 0;
         }
         this.char = this.character_set[this.cursor[this.cursor_pos]].char;
-        this.syllable = this.character_set[this.cursor[this.cursor_pos]].syllable;     
-        this.isfirstCharacter = this.cursor_pos === 0;
-        this.islastCharacter = this.character_set.length-1;  
+        this.syllable = this.character_set[this.cursor[this.cursor_pos]].syllable;        
+        return this.cursor_pos;
     }
     previous(){
         this.cursor_pos -= 1;
@@ -30,40 +27,19 @@ class Japanese {
         }
         this.char = this.character_set[this.cursor[this.cursor_pos]].char;
         this.syllable = this.character_set[this.cursor[this.cursor_pos]].syllable;
-        this.isfirstCharacter = this.cursor_pos === 0;
-        this.islastCharacter = this.character_set.length-1;  
+        return this.cursor_pos;
     }
     first(){
         this.cursor_pos = 0;
         this.char = this.character_set[this.cursor[this.cursor_pos]].char;
-        this.syllable = this.character_set[this.cursor[this.cursor_pos]].syllable;   
-        this.isfirstCharacter = this.cursor_pos === 0;
-        this.islastCharacter = this.character_set.length-1;  
+        this.syllable = this.character_set[this.cursor[this.cursor_pos]].syllable;        
+        return this.cursor_pos;
     }
     last(){
         this.cursor_pos = this.character_set.length-1;
         this.char = this.character_set[this.cursor[this.cursor_pos]].char;
-        this.syllable = this.character_set[this.cursor[this.cursor_pos]].syllable;
-        this.isfirstCharacter = this.cursor_pos === 0;
-        this.islastCharacter = this.character_set.length-1;  
-    }
-}
-
-class Hiragana extends Japanese {
-    constructor() {
-        super(getHiraganaJson());
+        this.syllable = this.character_set[this.cursor[this.cursor_pos]].syllable;        
+        return this.cursor_pos;
     }
 
   }
-class Katakana extends Japanese {
-    constructor() {
-        super(getKatakanaJson());
-    }
-
-  }  
-  class Kanji extends Japanese {
-    constructor() {
-        super(getKanjiJson());
-    }
-
-  } 
