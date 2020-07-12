43fba3c24b93fa10822efa5396f9f4ccd0a02e85
Peter Barraud <barraud@adobe.com>
2020-07-12 19:55:11 +0530

    versioning to see changes

--- index.lib.js
+++ index.lib.js
@@ -8,6 +8,8 @@ class Japanese {
         }
         this.char = this.character_set[this.cursor[this.cursor_pos]].char;
         this.syllable = this.character_set[this.cursor[this.cursor_pos]].syllable;
+        this.isfirstCharacter = this.cursor_pos === 0;
+        this.islastCharacter = this.character_set.length-1;  
     }
 
     next(){
@@ -16,7 +18,9 @@ class Japanese {
             this.cursor_pos = 0;
         }
         this.char = this.character_set[this.cursor[this.cursor_pos]].char;
-        this.syllable = this.character_set[this.cursor[this.cursor_pos]].syllable;        
+        this.syllable = this.character_set[this.cursor[this.cursor_pos]].syllable;     
+        this.isfirstCharacter = this.cursor_pos === 0;
+        this.islastCharacter = this.character_set.length-1;  
     }
     previous(){
         this.cursor_pos -= 1;
@@ -26,16 +30,22 @@ class Japanese {
         }
         this.char = this.character_set[this.cursor[this.cursor_pos]].char;
         this.syllable = this.character_set[this.cursor[this.cursor_pos]].syllable;
+        this.isfirstCharacter = this.cursor_pos === 0;
+        this.islastCharacter = this.character_set.length-1;  
     }
     first(){
         this.cursor_pos = 0;
         this.char = this.character_set[this.cursor[this.cursor_pos]].char;
-        this.syllable = this.character_set[this.cursor[this.cursor_pos]].syllable;        
+        this.syllable = this.character_set[this.cursor[this.cursor_pos]].syllable;   
+        this.isfirstCharacter = this.cursor_pos === 0;
+        this.islastCharacter = this.character_set.length-1;  
     }
     last(){
         this.cursor_pos = this.character_set.length-1;
         this.char = this.character_set[this.cursor[this.cursor_pos]].char;
-        this.syllable = this.character_set[this.cursor[this.cursor_pos]].syllable;        
+        this.syllable = this.character_set[this.cursor[this.cursor_pos]].syllable;
+        this.isfirstCharacter = this.cursor_pos === 0;
+        this.islastCharacter = this.character_set.length-1;  
     }
 }
 
